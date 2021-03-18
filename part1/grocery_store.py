78b72106be0ee31ec1ec59c970a4eac09b5e0ffe
Alex Boten <alrex.boten@gmail.com>
2021-03-18 15:33:06 -0700

    update shopper/grocery_store

--- part1/grocery_store.py
+++ part1/grocery_store.py
@@ -45,5 +45,20 @@ def whats_in_store():
     return res.text
 
 
+@app.route("/checkout")
+def checkout():
+    checkout_service = os.environ.get(
+        "CHECKOUT_SERVICE_URL", "http://localhost:8083/orders"
+    )
+    order = {
+        "items": [
+            {"id": "1", "quantity": 10},
+            {"id": "2", "quantity": 20},
+        ],
+    }
+    res = requests.post(checkout_service, json=order)
+    return res.text
+
+
 if __name__ == "__main__":
     app.run()
