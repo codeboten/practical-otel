ed7e9a27873a7d290af7334f35d21ead3fbab21d
Alex Boten <alrex.boten@gmail.com>
2021-03-19 19:31:23 -0700

    add manual instrumentation

--- part3/Checkout/src/main/java/com/example/checkout/CheckoutController.java
+++ part3/Checkout/src/main/java/com/example/checkout/CheckoutController.java
@@ -1,3 +1,4 @@
+// CheckoutController.java
 package com.example.checkout;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentelemetry.extension.annotations.WithSpan;
+import io.opentelemetry.api.trace.Span;
 
 @RestController
 public class CheckoutController {
@@ -15,14 +18,26 @@ public class CheckoutController {
     @Autowired
     private ApplicationContext context;
 
+    @WithSpan
+    String serializeOrder(Order order) {
+        Span span = Span.current();
+        span.setAttribute("order-id", order.id);
+        Jackson2JsonRedisSerializer<Order> serializer = new Jackson2JsonRedisSerializer<>(Order.class);
+        return new String(serializer.serialize(order));
+    }
+
+    @WithSpan
+    void sendMessage(String message) {
+        StringRedisTemplate template = context.getBean(StringRedisTemplate.class);
+        template.convertAndSend("orders", message);
+    }
+
     @PostMapping("/orders")
     Order newOrder(@RequestBody Order newOrder) {
         Order order = new Order();
         order.setItems(newOrder.getItems());
-        StringRedisTemplate template = context.getBean(StringRedisTemplate.class);
-
-        Jackson2JsonRedisSerializer<Order> serializer = new Jackson2JsonRedisSerializer<>(Order.class);
-        template.convertAndSend("orders", new String(serializer.serialize(order)));
+        String serializedOrder = serializeOrder(order);
+        sendMessage(serializedOrder);
         return order;
     }
 }
